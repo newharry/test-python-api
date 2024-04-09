fc9a9d48fd057834ed3e905fb1909d0ff47b5420
Henry Li <henryli05@gmail.com>
2024-04-09 03:06:11 -0700

    add my stuff

---
+++ app-test.py
@@ -0,0 +1,16 @@
+import requests
+import pandas as pd
+import time
+
+print('Begin!')
+
+myname = 'henry'
+
+url = f'https://api.nationalize.io/?name={myname}'
+
+r = requests.get(url).json()
+print(r)
+print(r.keys())
+
+df = pd.DataFrame(r['country'])
+print(df.sort_values(by=['probability'], ascending=False))
