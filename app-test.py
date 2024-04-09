9130b50777ea8fd2bf5d7b6034fa5a4346e39e4e
Henry Li <henryli05@gmail.com>
2024-04-09 03:36:11 -0700

    update

--- app-test.py
+++ app-test.py
@@ -2,16 +2,22 @@ import requests
 import pandas as pd
 import time
 
-print('Begin!')
+def lambda_handler(event, context):
+    print('Begin!')
 
-myname = 'henry'
+    myname = 'henry'
 
-url = f'https://api.nationalize.io/?name={myname}'
+    url = f'https://api.nationalize.io/?name={myname}'
 
-r = requests.get(url).json()
-print(r)
-print('Keys are:')
-print(r.keys())
+    r = requests.get(url).json()
+    print(r)
+    print('Keys are:')
+    print(r.keys())
 
-df = pd.DataFrame(r['country'])
-print(df.sort_values(by=['probability'], ascending=False))
+    df = pd.DataFrame(r['country'])
+    print(df.sort_values(by=['probability'], ascending=False))
+
+if __name__ == "__main__":
+    event = []
+    context = []
+    lambda_handler(event, context)
